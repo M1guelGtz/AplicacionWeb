354e8cc9d82c6d1aa99d887e5bdf779d03cab1b2
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-10-04 00:28:13 -0600

    add: creacion de el modulo home ademas del enrutado de la aplicacion, asi como la creacion de los componentes utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './Home/home.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
