8922b63f1a5367da5ed95bea12abca0a1df7ce62
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-11-01 12:19:58 -0600

    add: modificaciones

--- src/app/Services/contactos.service.ts
+++ src/app/Services/contactos.service.ts
@@ -8,8 +8,8 @@ export class ContactosService {
 
   constructor() { }
   contactos: Contacto[] = [
-    {id: 233382, name: 'Miguel Angel', email: 'Miguelangelgtzg@gmail.com', id_chat:1},
-    {id: 233377, name: 'Miguel Molina', email: 'Molina88@gmail.com', id_chat:2},
+    {id: 233382, name: 'Miguel Gutierrez', email: 'Miguelangelgtzg@gmail.com', id_chat: 1},
+    {id: 233377, name: 'Miguel Gomez', email: 'Insano88@gmail.com', id_chat:2},
     {id: 233371, name: 'Jose de jesus', email: 'JoseDJesusCorona@gmail.com', id_chat:3},
   ]
   eliminarContacto(id: number){
